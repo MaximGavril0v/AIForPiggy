0e92bba81b845bbc36b51dbb5438fb9bd0cc2c1b
Maxim Gavrilov <m4xim.gavril0v@gmail.com>
2020-10-15 02:11:54 +0300

    Trying to add avoiding player for animals...

--- src/main/java/com/maxgavr/feeder/events/ModClientEvents.java
+++ src/main/java/com/maxgavr/feeder/events/ModClientEvents.java
@@ -4,9 +4,7 @@ import com.maxgavr.feeder.Feeder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.AvoidEntityGoal;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.OcelotEntity;
-import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
@@ -28,9 +26,9 @@ public class ModClientEvents{
    @SubscribeEvent
     public static void avoidPlayer(EntityJoinWorldEvent event){
         Entity entity = event.getEntity();
-        if (entity instanceof AnimalEntity){
-            AnimalEntity animal = (AnimalEntity) entity;
-            animal.goalSelector.addGoal(10, new AvoidEntityGoal(animal, PlayerEntity.class, 16.0F, 0.8D, 1.33D));
+        if (entity instanceof PigEntity){
+            PigEntity pig = (PigEntity) entity;
+            pig.goalSelector.addGoal(2, new AvoidEntityGoal<>(pig, PlayerEntity.class, 16.0F, 0.8D, 1.33D));
         }
     }
 
