9edae1e9c78f5b7bb6b93bf894ec04622c582a39
Maxim Gavrilov <m4xim.gavril0v@gmail.com>
2020-10-15 01:38:17 +0300

    Trying to add avoiding player for animals...

--- src/main/java/com/maxgavr/feeder/events/ModClientEvents.java
+++ src/main/java/com/maxgavr/feeder/events/ModClientEvents.java
@@ -6,10 +6,15 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.OcelotEntity;
+import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.EntityPredicates;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,19 +23,30 @@ import net.minecraft.entity.ai.goal.GoalSelector;
 @Mod.EventBusSubscriber(modid = Feeder.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModClientEvents{
 
-    private GoalSelector goalSelector;
 
 
-    @SubscribeEvent
-    public void avoidPlayer(EntityJoinWorldEvent event){
+   @SubscribeEvent
+    public static void avoidPlayer(EntityJoinWorldEvent event){
         Entity entity = event.getEntity();
         if (entity instanceof AnimalEntity){
             AnimalEntity animal = (AnimalEntity) entity;
-            animal.goalSelector.addGoal(3, new net.minecraft.entity.ai.goal.AvoidEntityGoal(animal, PlayerEntity.class, 16.0F, 0.8D, 1.33D));
+            animal.goalSelector.addGoal(3, new AvoidEntityGoal(animal, PlayerEntity.class, 16.0F, 0.8D, 1.33D));
         }
-
-
     }
+
+   @SubscribeEvent
+    public static void onSheepDamage(AttackEntityEvent event){
+       if(event.getEntityLiving().getHeldItemMainhand().getItem() == Items.WHEAT){
+           if(event.getTarget().isAlive()){
+               LivingEntity target = (LivingEntity) event.getTarget();
+               if (target instanceof SheepEntity){
+                   target.addPotionEffect(new EffectInstance(Effects.POISON, 10*20));
+                   target.setGlowing(true);
+
+               }
+           }
+       }
+   }
 
 
 }
